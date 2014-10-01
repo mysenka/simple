a63b077288e80fec6a0c9a355fb06d8866e58755
mysenka <galina.amusina@gmail.com>
2014-10-01 13:19:05 -0400

    Initial

---
+++ myTest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyTest::Application.config.secret_key_base = '4510b51a120cf2ca82296d3930d99c9f634f1648f578090b015c8415a9ea4261fd127fa11a35b11393ff64dd0bfe0b1a88a8960881ec09127b4fc9dc1a344399'
---
+++ myTest/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyTest::Application.config.session_store :cookie_store, key: '_myTest_session'
